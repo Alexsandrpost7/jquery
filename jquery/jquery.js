df29cefa709625c45059b3479f3b72a5d58d3305
Alexsandrpost7 <76068301+Alexsandrpost7@users.noreply.github.com>
2021-03-17 08:07:43 +0300

    1 com

---
+++ jquery/jquery.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    //Code here  g
+    // alert("привет!"); +ok
+
+// Поменять цвет блока
+
+    // $(".color_test").css("color", "green");
+
+// Уведомление о свойстве
+    // alert($(".color_test").css("color",));
+
+// Изменени сво-ва при наведении
+    // $( ".color_test" ).on( "mouseover", function() {
+    //     $( this ).css( "color", "red" );
+    // });
+// При наведении меняет цвет, но один раз    
+    // $( ".color_test" ).hover( function() {
+    //     $( this ).css( "color", "red" );
+    // });
+
+// При клике менял цвет
+    // $(".color_click").click(function() {
+    //     $(this).css("color", "white");
+    // });
+    
+// При клике по любым дивам, кроме .color_click - меняем цвет
+    // $("div:not(.color_click)").click(function() {
+    //     $(".color_click").css("color", "white");
+    // });
+    
+// При клике по любым дивам, кроме .color_click - меняем цвет
+    
+
+});
+
+ 
